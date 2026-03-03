4179b1602456a6a7dbe76d09fe99fbbaaa01efbf
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-03-03 19:42:53 +0100

    Finished HexShapeContainer

--- scenes/hex_shape_container/hex_shape_container.gd
+++ scenes/hex_shape_container/hex_shape_container.gd
@@ -1,7 +1,7 @@
 class_name HexShapeContainer
 extends Container
 
-@export var min_hex_tile_size: Vector2:
+@export var min_hex_tile_size: Vector2 = Vector2(10, 10):
 	set(value):
 		min_hex_tile_size = value
 		for child in get_children():
@@ -14,17 +14,8 @@ func _init() -> void:
 
 
 func _on_child_entered_tree(child: Node) -> void:
-	if child is Map:
-		_set_child_position(child, (child as Map).get_shape())
-	elif child is Character:
-		_set_child_position(child, (child as Character).get_shape())
-
-
-func _set_hex_size_when_ready(child: Node, hex_size: Vector2) -> void:
-	if child.is_node_ready():
-		child.set_hex_size(hex_size)
-	else:
-		child.ready.connect(child.set_hex_size.bind(hex_size))
+	if child is Map or child is Character:
+		child.ready.connect(_set_child_position.bind(child, child.get_shape()))
 
 
 func _set_child_position(child: Node2D, shape: HexShape) -> void:
@@ -32,8 +23,9 @@ func _set_child_position(child: Node2D, shape: HexShape) -> void:
 	custom_minimum_size = min_board.size
 	
 	if !fill_size:
-		child.position = size/2 + min_board.position - min_board.size / 2
-		_set_hex_size_when_ready(child, min_hex_tile_size)
+		var min_middle := min_board.position + min_board.size / 2
+		child.position = size/2 - min_middle
+		child.set_hex_size(min_hex_tile_size)
 		return
 	
 	var max_board_scale := size / min_board.size
@@ -43,6 +35,7 @@ func _set_child_position(child: Node2D, shape: HexShape) -> void:
 	
 	var max_hex_tile_size := max_board_scale * min_hex_tile_size
 	var max_board := Hex.get_px_span(shape.hex_coordinates, max_hex_tile_size)
-	child.position = size/2 + max_board.position - max_board.size / 2
-	_set_hex_size_when_ready(child, max_hex_tile_size)
+	var max_middle := max_board.position + max_board.size / 2
+	child.position = size/2 - max_middle
+	child.set_hex_size(max_hex_tile_size)
 	
